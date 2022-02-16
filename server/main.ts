f393f827ea6bbd23879650c7633ccb685dbf9874
imilen <m.ivanov@email.com>
2022-02-16 23:33:43 +0200

    test(https): temporary disable for tests & actions

---
+++ server/main.ts
@@ -0,0 +1,37 @@
+import http from "http";
+import https from "https";
+import config from "config";
+
+import { log } from "./utils";
+import { mongoConnect } from "./db/mongo";
+import extractFromKS from "./certificate/https/generateKeys";
+import { app } from "./app";
+
+// extract configuration options
+const port = config.get<number>("port");
+const mongo = config.get<{ uri: string; options: object }>("mongo");
+
+// http server
+const httpServer = http.createServer(app);
+httpServer.listen(port);
+httpServer.on("listening", () => log.info(`server:listening ${port} 🔓`));
+httpServer.on("error", (error: any) =>
+  log.error(`server:error: ${error.message}`)
+);
+
+// const httpsKeys = extractFromKS();
+// const httpsServer = https.createServer(
+//   {
+//     cert: httpsKeys.cert,
+//     key: httpsKeys.key,
+//   },
+//   app
+// );
+// httpsServer.listen(443);
+// httpsServer.on("listening", () => log.info(`server:listening: 443 🔐`));
+// httpsServer.on("error", (error: any) =>
+//   log.error(`server:error: ${error.message}`)
+// );
+
+// database
+mongoConnect(mongo.uri, mongo.options);
