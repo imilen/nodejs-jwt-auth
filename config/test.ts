380d79760fb5a8e7bb4507d51fc261f3416a5104
imilen <m.ivanov@email.com>
2022-02-25 16:28:12 +0200

    perf(config): separate common envS into a separate file

--- config/test.ts
+++ config/test.ts
@@ -1,49 +1,9 @@
 import dotenv from "dotenv";
-import { Algorithm } from "jsonwebtoken";
+
+import { common } from "./common";
 
 dotenv.config({ encoding: "utf8" });
 
-export type mongoOptionsType = {
-  uri: string;
-  options: {};
-};
-
-export type redisOptionsType = {
-  host: string;
-  port: number;
-  pass: string;
-  db: number;
-};
-
-export type jwtOptionsType = {
-  accessTokenTtl: string;
-  refreshTokenTtl: string;
-  accessTokenFlag: string;
-  refreshTokenFlag: string;
-  algorithms: Algorithm[];
-};
-
 export default {
-  port: +JSON.stringify(process.env.PORT) || 80,
-  cookieName: JSON.stringify(process.env.COOKIE_NAME) || "sid",
-  cookieSecretKey: JSON.stringify(process.env.COOKIE_SECRET_KEY) || "$3cr37",
-  redis: {
-    host: JSON.stringify(process.env.REDIS_DB) || "localhost",
-    port: +JSON.stringify(process.env.REDIS_DB) || 6379,
-    pass: JSON.stringify(process.env.REDIS_DB) || "",
-    db: +JSON.stringify(process.env.REDIS_DB) || 0,
-  } as redisOptionsType,
-  mongo: {
-    uri:
-      JSON.stringify(process.env.MONGODB_URI) ||
-      "mongodb://localhost:27017/db?retryWrites=true&w=majority",
-    options: {},
-  } as mongoOptionsType,
-  jwt: {
-    accessTokenTtl: JSON.stringify(process.env.ACCESS_TOKEN_TTL) || "10m",
-    refreshTokenTtl: JSON.stringify(process.env.REFRESH_TOKEN_TTL) || "1h",
-    accessTokenFlag: JSON.stringify(process.env.ACCESS_TOKEN_FLAG) || "at",
-    refreshTokenFlag: JSON.stringify(process.env.REFRESH_TOKEN_FLAG) || "rt",
-    algorithms: ["RS256"],
-  } as jwtOptionsType,
+  ...common,
 };
